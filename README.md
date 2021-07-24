6f16a2e8e4658b2ce0e6c3f69c3b7929c959ea8a
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-07-24 11:12:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AdventOfCode2018
+ My solutions to Advent Of Code 2018 problems using C++17 and STL.
---
+++ cmake-build-release/CMakeFiles/AdventOfCode2018.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AdventOfCode2018.exe"
+  "AdventOfCode2018.exe.manifest"
+  "AdventOfCode2018.lib"
+  "AdventOfCode2018.pdb"
+  "CMakeFiles/AdventOfCode2018.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AdventOfCode2018.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
